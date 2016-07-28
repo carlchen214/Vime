803b4ec885aa73d60e43ee03457ab6ad6d547150
Carl Chen <carlchen214@gmail.com>
2016-07-27 21:47:39 -0700

    add new routes for new containers

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,14 +3,18 @@ import {Router, Route} from 'react-router';
 import App from './components/App.jsx';
 import Login from './containers/Login.jsx';
 import RecordQuestion from './containers/RecordQuestion.jsx';
-import VideoPlayer from './components/VideoPlayer.jsx';
+import RecordAnswer from './containers/RecordAnswer.jsx';
+import Topic from './containers/Topic.jsx';
+
+// import VideoPlayer from './components/VideoPlayer.jsx';
 import Error from './components/Error.jsx';
 
 export default (
   <Route path="/" component={App}>
     <Route path="login" component={Login} />
-    <Route path="record" component={RecordQuestion} />
-    <Route path="videos/:id" component={VideoPlayer} />
+    <Route path="record/question" component={RecordQuestion} />
+    <Route path="record/answer" component={RecordAnswer} />
+    <Route path="qa/:id" component={Topic} />
     <Route path="*" component={Error} />
   </Route>
 );
