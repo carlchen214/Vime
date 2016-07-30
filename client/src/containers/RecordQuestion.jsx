e1790a9046e91da6c78c672d11982721ed8b6a7c
Carl Chen <carlchen214@gmail.com>
2016-07-29 19:34:48 -0700

    Refactor for RecordQuestion to directly render tags, using Material UI

--- client/src/containers/RecordQuestion.jsx
+++ client/src/containers/RecordQuestion.jsx
@@ -3,36 +3,31 @@ import { connect } from 'react-redux';
 import { bindActionCreators} from 'redux';
 import { browserHistory } from 'react-router';
 import { addQuestion } from './../actions/questionAction.jsx';
-import SpeechTags from './../components/SpeechTags.jsx';
+import Chip from 'material-ui/Chip';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 
 
 class RecordQuestion extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      tags: []
+      removedTags: [],
+      tagData: []
     }
+    this.styles = {
+      chip: {
+        margin: 4
+      },
+      wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap'
+      }
+    };
     this.addToState = this.addToState.bind(this);
     this.addTags = this.addTags.bind(this);
-  }
-
-  addToState(data) {
-    var question = {
-      ...data,
-      userId: this.props.userId
-    }
-    // call action creator which will redirect to topic page
-    this.props.addQuestion(question);
-  }
-
-  addTags(data) {
-    var data = data.split(' ');
-    console.log('BEFORE FILTER', data);
-    data = data.filter(tag => tag.length > 4);
-    console.log('AFTER', data);
-    this.setState({
-      tags: data
-    });
   }
 
   render(){
@@ -40,11 +35,72 @@ class RecordQuestion extends React.Component {
       <div>
         <h1>What's your question?</h1>
         <Record addToState={this.addToState} apiUrl={'/api/questions'} userId={this.props.userId} addTags={this.addTags}/>
-        <SpeechTags tags={this.state.tags} />
+        <div>
+        {this.state.tagData.length > 0 ? <h4>I got some tags for ya:</h4> : null}
+          <MuiThemeProvider>
+          <div style={this.styles.wrapper}>
+            {this.state.tagData.map(this.renderTag, this)}
+          </div>
+          </MuiThemeProvider>
+        </div>
       </div>
     );
   }
+
+  renderTag(data) {
+    return (
+      <Chip
+        key={data.key}
+        onRequestDelete={() => this.handleRequestDelete(data.key)}
+        style={this.styles.chip}
+      >
+        {data.label}
+      </Chip>
+    );
+  }
+
+  addToState(data) {
+    var question = {
+      ...data,
+      userId: this.props.userId
+      // tags: this.state.tagData - this.state.removedTags
+    }
+    // call action creator which will redirect to topic page
+    this.props.addQuestion(question);
+  }
+
+  addTags(data) {
+    var data = data.split(' ');
+    // console.log('BEFORE FILTER', data);
+    data = data.filter(tag => tag.length > 4);
+    // use a better filter to parse out nouns, people, places
+    // http://nlp-compromise.github.io/website/#demos 
+    // console.log('AFTER', data);
+
+    var tagData = [];
+    for (var i = 0; i < data.length; i++) {
+      tagData.push({
+        key: i,
+        label: data[i]
+      }); 
+    }
+    this.setState({
+      tagData: tagData
+    });
+  }
+
+  handleRequestDelete(key) {
+    console.log('deleting tag');
+    var tagData = this.state.tagData;
+    const tagIndexToRemove = tagData.map((tag) => tag.key).indexOf(key);
+    var tagToRemove = tagData.splice(tagIndexToRemove, 1);
+    this.setState({
+      tagData: tagData,
+      removedTags: this.state.removedTags.concat([tagToRemove])
+    });
+  }
 }
+//        <SpeechTags tags={this.state.tags} removeTag={null} />
 
 
 function mapStateToProps(state) {
